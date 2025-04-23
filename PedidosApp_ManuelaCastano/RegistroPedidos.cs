2a4ceefdffbfacb38bc343d00c7250efb2749fcb
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-23 15:43:21 -0500

    Registro de pedidos con Singleton

---
+++ PedidosApp_ManuelaCastano/RegistroPedidos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp_ManuelaCastano
+{
+    public sealed class RegistroPedidos
+    {
+        private static RegistroPedidos _instance;
+        private static readonly object _lock = new object();
+        public List<Pedido> Pedidos { get; private set; }
+
+        private RegistroPedidos() => Pedidos = new List<Pedido>();
+
+        public static RegistroPedidos Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new RegistroPedidos();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void AgregarPedido(Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+        }
+    }
+}
+            
