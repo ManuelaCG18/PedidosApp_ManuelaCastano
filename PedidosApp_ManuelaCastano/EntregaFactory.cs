ab981dc18fffbb088a6d006c9b319123dee1b8f0
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-23 15:25:25 -0500

    Creacion del metodo Factory

---
+++ PedidosApp_ManuelaCastano/EntregaFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp_ManuelaCastano
+{
+    public static class EntregaFactory
+    {
+        public static IMetodoEntrega CrearEntrega(string TipoProducto, bool urgente, double peso)
+        {
+            if ( TipoProducto == "tecnología" && urgente)
+                return new EntregaDron();
+                else if (TipoProducto == "accesorio")
+                    return new EntregaMoto();
+                    else if (TipoProducto == "componente" || peso > 10)
+                        return new EntregaCamion();
+            else
+            {
+                return new EntregaMoto(); // es un valor por defecto
+            }
+            
+        }
+    }
+}
