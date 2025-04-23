c43623e550437c2c8bdd6a4409621e6707953dd0
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-23 15:06:24 -0500

    Creación interfaz de entrega

--- PedidosApp_ManuelaCastano/Form1.cs
+++ PedidosApp_ManuelaCastano/Form1.cs
@@ -16,5 +16,10 @@ namespace PedidosApp_ManuelaCastano
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ PedidosApp_ManuelaCastano/IMetodoEntrega.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp_ManuelaCastano
+{
+    public interface IMetodoEntrega
+    {
+        double CalcularCosto(int km);
+        string TipoEntrega();
+    }
+}
