76396ba62d3ee8f97202d8bcaf4e9594cbb5f922
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-23 16:53:28 -0500

    Programación de boton Calcular

--- PedidosApp_ManuelaCastano/Form1.Designer.cs
+++ PedidosApp_ManuelaCastano/Form1.Designer.cs
@@ -92,6 +92,7 @@
             this.btnCalcular.TabIndex = 5;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // lblResultado
             // 
--- PedidosApp_ManuelaCastano/Form1.cs
+++ PedidosApp_ManuelaCastano/Form1.cs
@@ -21,5 +21,26 @@ namespace PedidosApp_ManuelaCastano
         {
 
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cliente = txtCliente.Text;
+                string producto = cmbProducto.SelectedItem.ToString();
+                bool urgente = chkUrgente.Checked;
+                double peso = Convert.ToDouble(nudPeso.Value);
+                int distancia = Convert.ToInt32(nudDistancia.Value);
+
+                Pedido pedido = new Pedido(cliente, producto, urgente, peso, distancia);
+                RegistroPedidos.Instance.AgregarPedido(pedido);
+
+                lblResultado.Text = $"Entrega: {pedido.MetodoEntrega.TipoEntrega()}" + $"Costo: ${pedido.ObtenerCosto():0.00}";
+            }
+            catch (Exception ex) 
+                {
+                MessageBox.Show("Error: " + ex.Message);
+                }
+        }
     }
 }
